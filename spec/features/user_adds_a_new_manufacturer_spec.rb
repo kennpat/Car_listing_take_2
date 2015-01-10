09a29adcb57ba939eba264e245294b071547e91b
kennpat <kennedypr@gmail.com>
2015-01-10 12:25:50 -0500

    added manufacturer create and new with passing tests

---
+++ spec/features/user_adds_a_new_manufacturer_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+feature 'user adds a new manufacturer to the database', %{
+  As a user I want to add a new manufacturer
+  so that I can add vehicles from that manufacturer
+  to track my inventory available for sale.
+} do
+
+  scenario 'user posts manufacturer with correct info' do
+    visit new_manufacturer_path
+    fill_in 'Manufacturer Name', with: 'BMW'
+    fill_in 'Country of Manufacture', with: 'Germany'
+    click_on 'Submit'
+
+    expect(page).to have_content('Manufacturer posted successfully')
+    expect(page).to have_content('BMW')
+    expect(page).to have_content('Germany')
+  end
+
+  scenario 'user posts manufacturer with incorrect info' do
+    visit new_manufacturer_path
+    click_on 'Submit'
+
+    expect(page).to have_content("Name can't be blank")
+    expect(page).to have_content("Country can't be blank")
+
+  end
+
+end
