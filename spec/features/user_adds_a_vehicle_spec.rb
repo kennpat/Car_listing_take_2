cacd46d5cb7ea4a39f323476a20a11c9530a203a
kennpat <kennedypr@gmail.com>
2015-01-10 17:02:28 -0500

    added navigation features and a home page

--- spec/features/user_adds_a_vehicle_spec.rb
+++ spec/features/user_adds_a_vehicle_spec.rb
@@ -38,4 +38,22 @@ feature 'User adds a new vehicle to the inventory', %{
     expect(page).to have_content("Mileage can't be blank")
     expect(page).to have_content("Manufacturer can't be blank")
   end
+
+  scenario 'user tries to enter a really really old car - pre 1920' do
+    manufacturer1 = FactoryGirl.create(:manufacturer)
+    visit new_vehicle_path
+
+    vehicle = FactoryGirl.build(:vehicle)
+
+    fill_in 'Year', with: 1919
+    fill_in 'Color', with: vehicle.color
+    fill_in 'Mileage', with: vehicle.mileage
+
+    select manufacturer1.name, from: "Manufacturer"
+
+    click_on 'Submit'
+
+    expect(page).to have_content('Year must be greater than 1920')
+
+  end
 end
